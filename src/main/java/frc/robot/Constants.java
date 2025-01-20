c766c29e06286f47fba8faa3477414a5bc21cf78
MrNotSoPerfect <122anish.kumar@gmail.com>
2025-01-20 15:56:35 -0800

    to remove U

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,4 +48,7 @@ public final class Constants
     public static final double RIGHT_X_DEADBAND = 0.1;
     public static final double TURN_CONSTANT    = 6;
   }
+
+  public static int MotorContId = 10;
+
 }
