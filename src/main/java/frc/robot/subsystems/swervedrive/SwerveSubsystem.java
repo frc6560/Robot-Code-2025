846e76e4f1799a67ca311d16458953008d7a481e
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2025-02-12 17:54:47 -0800

    i hate this shitty ahh auto relative speeds fix

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -178,6 +178,9 @@ public class SwerveSubsystem extends SubsystemBase
           (speedsRobotRelative, moduleFeedForwards) -> {
             if (enableFeedforward)
             {
+              speedsRobotRelative = new ChassisSpeeds(speedsRobotRelative.vxMetersPerSecond,
+                                                      speedsRobotRelative.vyMetersPerSecond,
+                                                      -speedsRobotRelative.omegaRadiansPerSecond);
               swerveDrive.drive(
                   speedsRobotRelative,
                   swerveDrive.kinematics.toSwerveModuleStates(speedsRobotRelative),
