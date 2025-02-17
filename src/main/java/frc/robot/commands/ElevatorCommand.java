c3b4507a3276f4216597e7c789fa6d7e4796c6aa
Michael Wen <imichaelwen@gmail.com>
2025-02-16 20:18:37 -0800

    added l2

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -27,10 +27,14 @@ public class ElevatorCommand extends Command {
 
     @Override
     public void execute() {
-        if(controls.goToL2() || controls.goToL1()
+        if(controls.goToL1()
             || controls.goToPickup()
             || controls.goToStow()){
             targetState = State.STOW;
+
+        }
+        else if(controls.goToL2()){
+            targetState = State.L2;
 
         } else if(controls.goToL3()){
             targetState = State.L3;
@@ -46,6 +50,9 @@ public class ElevatorCommand extends Command {
 
         if (targetState == State.STOW) {
             targetrotelev = ElevatorConstants.ElevatorStates.STOW;
+        }
+        else if (targetState == State.L2) {
+            targetrotelev = ElevatorConstants.ElevatorStates.L2;
         }
         else if (targetState == State.L3) {
             targetrotelev = ElevatorConstants.ElevatorStates.L3;
