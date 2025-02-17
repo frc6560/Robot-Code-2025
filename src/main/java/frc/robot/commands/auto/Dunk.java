03baff4491a3c4093f2f9a1ddd0efb6bf7ddd047
Michael Wen <imichaelwen@gmail.com>
2025-02-16 16:45:31 -0800

    i hate this

---
+++ src/main/java/frc/robot/commands/auto/Dunk.java
@@ -0,0 +1,38 @@
+package frc.robot.commands.auto;
+
+import frc.robot.subsystems.Wrist;
+import frc.robot.subsystems.Wrist.State;
+import frc.robot.Constants.WristConstants;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Dunk extends Command{
+    private Wrist wrist;
+    private int outtake = -1;
+
+    public Dunk(Wrist wrist){
+        this.wrist = wrist;
+
+        addRequirements(wrist);
+    }
+
+    @Override
+    public void initialize(){
+    }
+
+    @Override
+    public void execute(){
+        wrist.setMotorPosition(WristConstants.WristStates.L4 + outtake * WristConstants.WristStates.L4Offset);
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        wrist.stopMotor();
+    }
+
+    @Override
+    public boolean isFinished(){
+        final double tolerance = 0.1;
+        return Math.abs(wrist.getWristAngle() - WristConstants.WristStates.L4 + outtake * WristConstants.WristStates.L4Offset) < tolerance;
+    }
+}
