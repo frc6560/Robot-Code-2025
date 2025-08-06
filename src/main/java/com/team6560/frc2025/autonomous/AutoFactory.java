794be2f74418aa13950b6eb505730a8dfeca3020
Michael Wen <imichaelwen@gmail.com>
2025-08-06 12:18:05 -0700

    auto folder

---
+++ src/main/java/com/team6560/frc2025/autonomous/AutoFactory.java
@@ -0,0 +1,8 @@
+package com.team6560.frc2025.autonomous;
+
+import com.team6560.frc2025.commands.AutoAlignCommand;
+import com.team6560.frc2025.commands.IntakeCommand;
+
+public class AutoFactory {
+    
+}
---
+++ src/main/java/com/team6560/frc2025/autonomous/AutoRoutines.java
@@ -0,0 +1,11 @@
+package com.team6560.frc2025.autonomous;
+
+/** Just a list of autos, subject to change at any time */
+public enum Auto {
+    ADAPTIVE,
+    CENTER3P,
+    LEFT3P,
+    RIGHT3P,
+    LEFT4PL3,
+    RIGHT4PL3
+}
