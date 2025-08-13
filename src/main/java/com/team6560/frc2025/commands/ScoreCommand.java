cffa50cb98fd090aa7223c0a57fd5500c1c1ccec
kevin chou <Kevinurmomchou@icloud.com>
2025-08-13 16:40:23 -0700

    added algae score commands

--- src/main/java/com/team6560/frc2025/commands/ScoreCommand.java
+++ src/main/java/com/team6560/frc2025/commands/ScoreCommand.java
@@ -82,7 +82,7 @@ public class ScoreCommand extends SequentialCommandGroup {
 
     /** A constructor to score at a given level... in teleoperated mode.*/
     public ScoreCommand(Wrist wrist, Elevator elevator, PipeGrabber grabber, SwerveSubsystem drivetrain, 
-                            ReefSide side, ReefIndex location, ReefLevel level, boolean isAuto) {
+                            ReefSide side, ReefIndex location, ReefLevel level, boolean isAuto, AlgaeLevel algaeLevel) {
 
         this.drivetrain = drivetrain;
         this.wrist = wrist;
