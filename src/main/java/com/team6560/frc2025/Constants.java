11df851ab99002185b6e4e83cc4b9b5102a01015
kevin chou <Kevinurmomchou@icloud.com>
2025-09-13 21:55:12 -0700

    dereef command

--- src/main/java/com/team6560/frc2025/Constants.java
+++ src/main/java/com/team6560/frc2025/Constants.java
@@ -158,8 +158,10 @@ public final class Constants {
     public static final Pose2d RIGHT_BLUE = new Pose2d(BLUE_X, 2.636, Rotation2d.fromDegrees(0));
     public static final Pose2d CENTER_BLUE = new Pose2d(BLUE_X, 4.0, Rotation2d.fromDegrees(0));
 
-    public static final double top_algae = 0.0;
-    public static final double low_algae = 0.0;
+    public static final double Elevator_top_algae = 0.0;
+    public static final double Wrist_top_algae = 0.0;
+    public static final double Elevator_low_algae = 0.0;
+    public static final double Wrist_low_algae = 0.0;
 
     public static Pose2d getFarRight(DriverStation.Alliance alliance){
       return alliance == DriverStation.Alliance.Red ? FAR_RIGHT : FAR_RIGHT_BLUE;
