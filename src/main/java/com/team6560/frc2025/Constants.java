644f8c6f4f0c81514ae55728b37a1b219ad6f7f1
team6560 <141588257+team6560@users.noreply.github.com>
2025-09-14 18:34:59 -0700

    pid changes

--- src/main/java/com/team6560/frc2025/Constants.java
+++ src/main/java/com/team6560/frc2025/Constants.java
@@ -54,9 +54,9 @@ public final class Constants {
     public static final double kI_translation = 0; 
     public static final double kD_translation = 0.15; 
 
-    public static final double kP_rotation = 3.0;
+    public static final double kP_rotation = 2;
     public static final double kI_rotation = 0;
-    public static final double kD_rotation = 0.15;
+    public static final double kD_rotation = 0.2;
 
     public static final double kStdvX = 0.08;
     public static final double kStdvY = 0.08;
