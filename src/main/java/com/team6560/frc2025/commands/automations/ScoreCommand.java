c9c92fd0ee982bb3018a8986f10b89fef95c2743
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2025-09-24 00:31:13 -0700

    cleanup

--- src/main/java/com/team6560/frc2025/commands/automations/ScoreCommand.java
+++ src/main/java/com/team6560/frc2025/commands/automations/ScoreCommand.java
@@ -24,7 +24,6 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
