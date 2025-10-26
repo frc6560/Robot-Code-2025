ce23a36bf182309736ee920e3886ad82a2e4b64a
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2025-10-25 21:20:05 -0700

    hi

--- src/main/java/com/team6560/frc2025/autonomous/AutoFactory.java
+++ src/main/java/com/team6560/frc2025/autonomous/AutoFactory.java
@@ -102,8 +102,8 @@ public class AutoFactory {
                 scoreFactory.getScoreAuto(ReefSide.LEFT, "4pl1", ReefLevel.L4),
                 new IntakeCommand(wrist, elevator, drivetrain, "4pl2"),
                 scoreFactory.getScoreAuto(ReefSide.RIGHT, "4pl3", ReefLevel.L4),
-                new IntakeCommand(wrist, elevator, drivetrain, "4pl4"),
-                scoreFactory.getScoreAuto(ReefSide.LEFT, "4pl5", ReefLevel.L4)
+                new IntakeCommand(wrist, elevator, drivetrain, "4pl4")
+                // scoreFactory.getScoreAuto(ReefSide.LEFT, "4pl5", ReefLevel.L4)
             ), Set.of(wrist, elevator, grabber, drivetrain))
         );
     }
