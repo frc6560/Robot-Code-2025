fede811a2f3d1e3f79039d1530ba6e85c6dfb0ac
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2025-10-26 16:24:57 -0700

    full four piece left

--- src/main/java/com/team6560/frc2025/autonomous/AutoFactory.java
+++ src/main/java/com/team6560/frc2025/autonomous/AutoFactory.java
@@ -103,7 +103,9 @@ public class AutoFactory {
                 new IntakeCommand(wrist, elevator, drivetrain, "4pl2"),
                 scoreFactory.getScoreAuto(ReefSide.LEFT, "4pl3", ReefLevel.L4),
                 new IntakeCommand(wrist, elevator, drivetrain, "4pl4")
-                // scoreFactory.getScoreAuto(ReefSide.LEFT, "4pl5", ReefLevel.L4)
+                // scoreFactory.getScoreAuto(ReefSide.RIGHT, "4pl5", ReefLevel.L4)
+                // new IntakeCommand(wrist, elevator, drivetrain, "4pl6"),
+                // scoreFactory.getScoreAuto(ReefSide.LEFT, "4pl7", ReefLevel.L4)
             ), Set.of(wrist, elevator, grabber, drivetrain))
         );
     }
